618b7745df782d4a0d8f2a38be71b5bf9de21bb1
Vojtech Bocek <vbocek@gmail.com>
2018-07-09 10:07:01 +0200

    Add script to automatically run uploadfs when needed

---
+++ pre_extra_script.py
@@ -0,0 +1,28 @@
+import os
+import hashlib
+
+Import("env", "projenv")
+
+def before_upload(source, target, env):
+    hasher = hashlib.sha1()
+    for root, dirs, files in os.walk("data"):
+        dirs.sort()
+        for name in sorted(files):
+            with open(os.path.join(root, name), "rb") as f:
+                for chunk in iter(lambda: f.read(32787), b""):
+                    hasher.update(chunk)
+
+    current_sha1 = hasher.hexdigest()
+    if os.path.exists(".last_uploadfs_sha1"):
+        with open(".last_uploadfs_sha1", "r") as f:
+            if f.read() == current_sha1:
+                print("SPIFFS data are the same.")
+                return
+    
+    print("SPIFFS data changed, running uploadfs target!")
+    with open(".last_uploadfs_sha1", "w") as f:
+        f.write(current_sha1)
+
+    env.Execute("pio run -t uploadfs")
+
+env.AddPreAction("upload", before_upload)
