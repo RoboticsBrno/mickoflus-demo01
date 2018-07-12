443ff6873ae7b3bce73c728ec1017e39a014a005
Vojtech Bocek <vbocek@gmail.com>
2018-07-12 09:13:32 +0200

    Implement wifi AP mode

--- src/main.cpp
+++ src/main.cpp
@@ -35,12 +35,21 @@ void onPktReceived(rb::Protocol& protocol, void *cookie, const std::string& comm
 }
 
 extern "C" void app_main() {
+    // Initialize the robot manager
+    rb::Manager man;
+
     // Connect to the WiFi network
-    rb::WiFi::connect(WIFI_NAME, WIFI_PASSWORD);
+    // If the button 1 is not pressed: connect to WIFI_NAME
+    // else create an AP.
+    if(man.expander().digitalRead(rb::SW1) != 0) {
+        man.leds().yellow();
+        rb::WiFi::connect(WIFI_NAME, WIFI_PASSWORD);
+    } else {
+        man.leds().green();
+        rb::WiFi::startAp("Flus" OWNER "-" NAME, "flusflus");
+    }
 
     rb_web_start(80);   // Start web server with control page (see data/index.html)
-
-    rb::Manager man;    // Initialize the robot manager
 
     // Set motor power limits
     man.setMotors()
@@ -55,13 +64,6 @@ extern "C" void app_main() {
     prot.start();
 
     printf("%s's mickoflus '%s' started!\n", OWNER, NAME);
-
-    // Turn on the LED according to pressed buttons
-    if(man.expander().digitalRead(rb::SW1) != 0) {
-        man.leds().yellow();
-    } else {
-        man.leds().green();
-    }
 
     int i = 0;
     const auto& bat = man.battery();
