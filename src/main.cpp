003ddd7fb80f759bb6b5a07074c9a42444834034
Vojtech Bocek <vbocek@gmail.com>
2018-12-25 17:11:14 +0100

    Add arm controls

--- src/main.cpp
+++ src/main.cpp
@@ -15,22 +15,26 @@
 
 #include "motors.hpp"
 
+using namespace std::chrono_literals; // to use ms/s/h etc.
+#include "lx16a.hpp"
+
 // CHANGE THESE so you can find the robot in the Android app
 #define OWNER "FrantaFlinta"
 #define NAME "FlusMcFlusy"
 
 // CHANGE THESE to your WiFi's settings
-#define WIFI_NAME "Mickoland"
-#define WIFI_PASSWORD "flusflus"
-
+#define WIFI_NAME "domov"
+#define WIFI_PASSWORD "Monty2aTara"
 
 void onPktReceived(rb::Protocol& protocol, void *cookie, const std::string& command, rbjson::Object *pkt) {
     auto man = (rb::Manager*)cookie;
     if(command == "joy") {
         motors_handle_joysticks(man, pkt);
-    } else if(command == "fire") {
-        printf("\n\nFIRE THE MISSILESS\n\n");
-        motors_fire_gun(man);
+    } else if(command == "arm") {
+        const rbjson::Array *data = pkt->getArray("data");
+        const rbjson::Array *angles = data->getObject(0)->getArray("a");
+        man->servoBus().set(0, angles->getDouble(0, 0), 130);
+        man->servoBus().set(1, angles->getDouble(1, 0), 130);
     }
 }
 
@@ -38,11 +42,15 @@ extern "C" void app_main() {
     // Initialize the robot manager
     rb::Manager man;
 
+    auto& servos = man.initServoBus(2);
+    servos.limit(0,  0_deg, 180_deg );
+    servos.limit(1,  6_deg, 240_deg );
+
     // Set the battery measuring coefficient.
     // Measure voltage at battery connector and
     // coef = voltageMeasureAtBatteriesInMilliVolts / raw
     auto& batt = man.battery();
-    batt.setCoef(9.0);
+    batt.setCoef(10);
 
     // Connect to the WiFi network
     // If the button 1 is not pressed: connect to WIFI_NAME
@@ -61,8 +69,6 @@ extern "C" void app_main() {
     man.setMotors()
         .pwmMaxPercent(MOTOR_LEFT, 70)  // left wheel
         .pwmMaxPercent(MOTOR_RIGHT, 70)  // right wheel
-        .pwmMaxPercent(MOTOR_TURRET_ROTATION, 28)  // turret left/right
-        .pwmMaxPercent(MOTOR_TURRET_PITCH, 45)  // turret up/down
         .set();
 
     // Initialize the communication protocol
@@ -74,9 +80,13 @@ extern "C" void app_main() {
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     printf("\n\nBATTERY CALIBRATION INFO: %d (raw) * %.2f (coef) = %dmv\n\n\n", batt.raw(), batt.coef(), batt.voltageMv());
 
+    //servos.set(0, 180, 100);
+    //servos.set(1, 0, 100);
+
     int i = 0;
     while(true) {
         vTaskDelay(1000 / portTICK_PERIOD_MS);
+
         if(prot.is_possessed()) {
             // Send text to the android application
             prot.send_log("Tick #%d, battery at %d%%, %dmv\n", i++, batt.pct(), batt.voltageMv());
--- src/motors.cpp
+++ src/motors.cpp
@@ -15,17 +15,13 @@ static int scale_range(float x, float omin, float omax, float nmin, float nmax) 
     return ((float(x) - omin) / (omax - omin)) * (nmax - nmin) - nmax;
 }
 
-static int scale_range_decel(float x, float omin, float omax, float nmin, float nmax) {
-    return scale_decel((float(x) - omin) / (omax - omin)) * (nmax - nmin) - nmax;
-}
-
 static int scale_motors(float val) {
     return scale_range(val, RBPROTOCOL_AXIS_MIN, RBPROTOCOL_AXIS_MAX, -100.f, 100.f);
 }
 
 void motors_handle_joysticks(rb::Manager *man, rbjson::Object *pkt) {
     const rbjson::Array *data = pkt->getArray("data");
-        
+
     auto builder = man->setMotors();
 
     // Drive
@@ -48,41 +44,5 @@ void motors_handle_joysticks(rb::Manager *man, rbjson::Object *pkt) {
         builder.power(MOTOR_LEFT, l).power(MOTOR_RIGHT, r);
     }
 
-    // Turret
-    {
-        const rbjson::Object *joy = data->getObject(1);
-        int x = joy->getInt("x") * -1;
-        int y = joy->getInt("y");
-
-        if(iabs(x) < 6000) {
-            x = 0;
-        } else {
-            x = scale_range_decel(x, RBPROTOCOL_AXIS_MIN, RBPROTOCOL_AXIS_MAX, -100, 100);
-        }
-
-        if(x != 0 || iabs(y) < 6000) {
-            y = 0;
-        } else{
-            if(y > 0)
-                y = scale_range_decel(y, RBPROTOCOL_AXIS_MIN, RBPROTOCOL_AXIS_MAX, -100, 100);
-            else
-                y = scale_range_decel(y, RBPROTOCOL_AXIS_MIN, RBPROTOCOL_AXIS_MAX, -75, 75);
-        }
-
-        builder.power(MOTOR_TURRET_ROTATION, x).power(MOTOR_TURRET_PITCH, y);
-    }
-
     builder.set();
-}
-
-
-static bool turnOffGun(void *cookie) {
-    auto man = (rb::Manager*)cookie;
-    man->setMotors().power(MOTOR_GUN, 0).set();
-    return false;
-}
-
-void motors_fire_gun(rb::Manager *man) {
-    man->setMotors().power(MOTOR_GUN, 100).set();
-    man->schedule(3000, &turnOffGun, man);
 }
