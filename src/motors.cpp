ead3078deda7c798c84bce1029139d3649c8fac1
Vojtech Bocek <vbocek@gmail.com>
2019-05-20 12:01:33 +0200

    Fix motors

--- src/motors.cpp
+++ src/motors.cpp
@@ -3,20 +3,8 @@
 
 #include "motors.hpp"
 
-static int iabs(int x) {
-    return x >= 0 ? x : -x;
-}
-
-static float scale_decel(float x) {
-    return (1.f - (1.f - x) * (1.f - x));
-}
-
 static int scale_range(float x, float omin, float omax, float nmin, float nmax) {
     return ((float(x) - omin) / (omax - omin)) * (nmax - nmin) - nmax;
-}
-
-static int scale_range_decel(float x, float omin, float omax, float nmin, float nmax) {
-    return scale_decel((float(x) - omin) / (omax - omin)) * (nmax - nmin) - nmax;
 }
 
 static int scale_motors(float val) {
@@ -26,7 +14,7 @@ static int scale_motors(float val) {
 void motors_handle_joysticks(rb::Manager& man, rbjson::Object *pkt) {
     const rbjson::Array *data = pkt->getArray("data");
 
-    if(data->size() < 2) {
+    if(data->size() < 1) {
         return;
     }
 
@@ -51,22 +39,6 @@ void motors_handle_joysticks(rb::Manager& man, rbjson::Object *pkt) {
 
         builder.power(MOTOR_LEFT, l).power(MOTOR_RIGHT, r);
     }
-
-    // Turret
-    {
-        const rbjson::Object *joy = data->getObject(1);
-        int x = joy->getInt("x") * -1;
-        int y = joy->getInt("y");
-
-        if(iabs(x) < 6000) {
-            x = 0;
-        } else {
-            x = scale_range_decel(x, RBPROTOCOL_AXIS_MIN, RBPROTOCOL_AXIS_MAX, -100, 100);
-        }
-
-        builder.power(MOTOR_TURRET_ROTATION, x);
-    }
-
 
     builder.set();
 }
